e7ee4bf24e6e7d7a3eadef32bb7702c766e37164
lwh9346 <lwh9346@163.com>
2020-11-28 16:43:27 +0800

    修改gitignore、修改编译脚本

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@ mkdir build
 set GOOS=windows
 go build -o ./build/AutoUpdater.exe ./updater/main.go
 go build -o ./build/SelfUpdater.exe ./selfupdate/main.go
+strip ./build/AutoUpdater.exe
 strip ./build/SelfUpdater.exe
 upx -9 ./build/SelfUpdater.exe
 upx -9 ./build/AutoUpdater.exe
