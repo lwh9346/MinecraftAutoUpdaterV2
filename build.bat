03a720b3bcdeafe830e26438936322e8ad7dd41f
lwh9346 <lwh9346@163.com>
2024-07-08 21:38:16 +0800

    取消upx加壳等缩减文件体积的处理，避免误报毒

--- build.bat
+++ build.bat
@@ -2,11 +2,6 @@ mkdir program
 set GOOS=windows
 go build -o ./program/AutoUpdater.exe ./updater/main.go
 go build -o ./program/SelfUpdater.exe ./selfupdate/main.go
-strip ./program/AutoUpdater.exe
-strip ./program/SelfUpdater.exe
-upx -9 ./program/SelfUpdater.exe
-upx -9 ./program/AutoUpdater.exe
 program\AutoUpdater.exe hash
 set GOOS=linux
 go build -o ./program/proxy ./pserver/main.go
-upx -9 ./program/proxy
--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	ResourceURL     = "https://minecraft-updater.oss-accelerate.aliyuncs.com"
-	UseProxy        = true
+	UseProxy        = false
 	ProxyListenAddr = "127.0.0.1:25565"
 	ProxyConnectTo  = "ddns.whl.red:25565"
 )
